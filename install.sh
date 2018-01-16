a00ea44b7f06a219b2e34a2a1ed8878d90f0855b
Helmut Januschka <helmut@januschka.com>
2018-01-16 23:01:12 +0100

    UP

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc gvimrc tmux.conf tmuxinator"    # list of files/folders to symlink in homedir
+files="vimrc gvimrc tmux.conf tmuxinator tmux.conf.local"    # list of files/folders to symlink in homedir
 
 ##########
 
